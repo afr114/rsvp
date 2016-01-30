014a852e0944942f10b4e275829463c9ad73eeb0
Jake Glassman <jake.glassman@gmail.com>
2016-01-30 10:28:21 -0800

    add photos to users and events plus new and edit views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
+  has_attached_file :photo, styles: {large: "800x800>" medium: "300x200#", thumb: "100x100#" }, default_url: "/assets/images/missing.jpg"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   has_many :guests
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130052711) do
+ActiveRecord::Schema.define(version: 20160130182720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20160130052711) do
     t.integer  "user_id"
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "guests", force: :cascade do |t|
@@ -62,6 +66,10 @@ ActiveRecord::Schema.define(version: 20160130052711) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
