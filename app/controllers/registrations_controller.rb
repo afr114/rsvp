89ca4e1cdbd12680ea9ea73ca5f3e436bde897ed
Aileen Cacayorin <acacayorin@gmail.com>
2016-01-30 15:29:02 -0800

    more sign up stuff

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
-  protected
 
+  def create
+    super do |resource|
+        if resource.save
+          binding.pry
+          @event_id = params[:event_id]
+          binding.pry
+          @event = Event.find(@event_id.to_i)
+          @guest = Guest.new
+          @guest.event = @event
+          @event.user = current_user
+          @guest.save
+      end
+    end
+  end
+
+  protected
   def after_sign_up_path_for(resource)
     'user/:id' # Or :prefix_to_your_route
   end
