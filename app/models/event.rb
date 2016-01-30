baa2a655c172ae9477a1dd912a08852abdea2065
Jake Glassman <jake.glassman@gmail.com>
2016-01-30 10:54:04 -0800

    'photo instead of avatar lol'

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
-  has_attached_file :photo, styles: {large: "800x800>" medium: "300x200#", thumb: "100x100#" }, default_url: "/assets/images/missing.jpg"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  has_attached_file :photo, styles: {large: "800x800>", medium: "300x200#", thumb: "100x100#" }
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   has_many :guests
   has_many :locations
