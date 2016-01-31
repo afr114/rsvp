ae4d8624a59df096def753ab127c3ac6f775e3f2
Aileen Cacayorin <acacayorin@gmail.com>
2016-01-31 09:45:41 -0800

    more heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_classes = true
+  
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -22,14 +25,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -80,4 +83,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
 
   config.action_controller.default_url_options = { host: 'rsvp-list.herokuapp.com/' }
+
 end
